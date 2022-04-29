83a86c87eb610e45189679ffcce3c423c94a35be
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2022-04-29 09:19:08 +0300

    ff

--- 28_04_2022/28_04_task4.js
+++ 28_04_2022/28_04_task4.js
@@ -2,11 +2,22 @@
 //  Range within
 let input = process.argv[2];
 let input2 = process.argv[3];
-let msg = "";
-if (input > input2) {
-    for (let i = input; i <= input2; i++) {
-        msg = msg + i + " ";
+let msg = [];
+if (input < input2) {
+
+	for (let i = input; i <= input2; i++) {
+		;
+		// msg.push(+i); //unary operator fucks negative numbers
+		msg.push(Number(i)); //unary operator
+
 	}
 
+}
+else {
+	for (let i = input2-1; i <= input; i++) {
+
+		msg.push(Number(i));
+		msg.reverse(); //doh, reverse the array
+	}
 }
 	console.log(msg);
---
+++ 28_04_2022/28_04_task5.js
@@ -0,0 +1,8 @@
+//Antti Suomi 27.4
+//  isPalindrome
+let input = process.argv[2];
+let msg = `"${input}"`;
+arr = 
+
+
+console.log(msg);
