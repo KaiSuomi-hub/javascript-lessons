e3ecf935020e1721187085e4fae8c31a7d0d6b31
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2022-05-03 11:05:36 +0300

    Update 3_5_onsite_task3.js

--- 3_5_onsite/3_5_onsite_task3.js
+++ 3_5_onsite/3_5_onsite_task3.js
@@ -3,11 +3,8 @@
 let num = Number(process.argv[2]);
 let num2 = 1;
 let f = function (num) {
-
-
 	if (num >=  1) {
 		num2 = num * num2;
-
 	return f(num - 1); // calling itself
 	} else {
 			console.log(num2); //stops calling itself
