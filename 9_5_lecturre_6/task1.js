120a72645fdfe0457ac699467c26a0abad77b8d0
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2022-05-09 10:03:36 +0300

    classwork 1000

---
+++ 9_5_lecturre_6/task1.js
@@ -0,0 +1,16 @@
+const anotherFunction = (time, next) => {
+	setTimeout(next,time);
+}
+
+anotherFunction(1000, () => {
+	console.log("three")
+	anotherFunction(1000, () => {
+	console.log("two")
+	anotherFunction(1000, () => {
+	console.log("one");
+	anotherFunction(1000, () => {
+	console.log("go")
+	});
+	});
+	});
+});
