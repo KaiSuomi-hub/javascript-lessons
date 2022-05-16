e05da7e2ab978c1251c9cc95ac0c1bb7dd3c301e
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2022-05-16 13:11:33 +0300

    GG

---
+++ 00EXAMPLES/axios_example.js
@@ -0,0 +1,36 @@
+//Antti Suomi 16.5
+
+const axios = require("axios");
+
+const getData = async (url) => {
+    const data = (await axios.get(url)).data; //here we create a new function that get's the json data
+    return data;
+};
+
+const addr = "https://jsonplaceholder.typicode.com/todos/";
+const userId = async (id) =>
+    getData(`https://jsonplaceholder.typicode.com/users/${id}`); //he we get data per user id
+
+async function main() {
+    const data = await getData(addr);
+    //  console.log(data);
+    const users = await getData("https://jsonplaceholder.typicode.com/users/");
+    const usersObj = {};
+    users.forEach((u) => (usersObj[u.id] = u));
+    // console.log(users.userId);
+    for (const post of data) {
+        const id = post.userId;
+        const { name, username, email } = usersObj[id];
+        delete post.userId;
+        post.user = {
+            name,
+            username,
+            email,
+        };
+    }
+
+    console.log(data);
+}
+
+main();
+//thx tuoppis
--- 9_5_lecturre_6/3task.js
+++ 9_5_lecturre_6/3task.js
@@ -5,5 +5,33 @@ const getValue = function () {
         }, Math.random() * 1500);
     });
 };
-let result = async (getValue()) ;
-console.log(result);
+
+// promises
+let valueOneHere;
+let valueTwoHere;
+
+getValue()
+    .then((ret) => {
+        valueOneHere = ret.value;
+        return getValue();
+    }).then((ret) => {
+        valueTwoHere = ret.value;
+        console.log(`Value 1 is ${valueOneHere} and value 2 is ${valueTwoHere}`);
+    });
+
+// promise.all + async/await
+
+const func = async function() {
+    const ret = await Promise.all([getValue(), getValue()]);
+    console.log(`Value 1 is ${ret[0].value} and value 2 is ${ret[1].value}`);
+}
+func();
+
+// async/await
+const func2 = async function() {
+    const valueOne = await getValue();
+    const valueTwo = await getValue();
+    console.log(`Value 1 is ${valueOne.value} and value 2 is ${valueTwo.value}`);
+}
+
+func2();
