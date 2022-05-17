c851a4b5de0ec0567faf72245ad5741885d0ab60
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2022-05-17 14:51:38 +0300

    ss

--- 17_5_lecture_7_remote/src/javascript/output.js
+++ 17_5_lecture_7_remote/src/javascript/output.js
@@ -1,20 +1,13 @@
 // 17.5.2022 Antti Suomi
 
-import { axios } from '../../node_modules/axios/dist/axios.js';
+import axios from 'axios';
 
-axios;
+const getData = async (url) => {
+    const data = (await axios.get(url)).data; //here we create a new function that get's the json data
+    return data;
+};
+const addr = "../data/data.json";
 
-// const jsonString = JSON.stringify(forecast);
-
-// fs.writeFile("./forecast-data.json", jsonString, (err) => {
-//     if (err) {
-//         console.log("Error writing file", err);
-//     } else {
-//         console.log("Successfully wrote file");
-//     }
-// });
-
-// const data = require('./forecast-data.json');
 const output = (needThisHtml) => {
 
 
