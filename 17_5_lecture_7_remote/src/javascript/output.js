1e47babb32aea5515a9b3d67a37f1a0233d239bc
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2022-05-17 20:18:03 +0300

    Cant seem to get axios or fetch to work

--- 17_5_lecture_7_remote/src/javascript/output.js
+++ 17_5_lecture_7_remote/src/javascript/output.js
@@ -1,16 +1,36 @@
 // 17.5.2022 Antti Suomi
 
 import axios from 'axios';
-
-const getData = async (url) => {
-    const data = (await axios.get(url)).data; //here we create a new function that get's the json data
-    return data;
-};
-const addr = "../data/data.json";
-
-const output = (needThisHtml) => {
+import 'regenerator-runtime/runtime';
+const addr = 'http://localhost:1234/data.json';
 
 
+const output = () => {
+
+async function getapi(url) {
+
+    // Storing response
+    const response = await fetch(url);
+
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data.commands);
+
+    // show(data);
 }
+
+
+
+
+	// let gg = fetch(addr);
+	// console.log(gg)
+	// let ff = JSON.parse(gg);
+	// console.log(ff);
+	// return ff;
+	getapi(addr);
+}
+
 export { output };
-let outputJson = JSON.parse(jsonString); //* here we read the data to object(?)
+
+
+// let outputJson = JSON.parse(jsonString); //* here we read the data to object(?)
