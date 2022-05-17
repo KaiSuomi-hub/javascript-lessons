2df3a781e82d2c146c6803ea31f34185725fa5a2
KaiSuomi-hub <suomen.perhe@gmail.com>
2022-05-17 13:17:05 +0300

    ss

--- 17_5_lecture_7_remote/src/javascript/index.js
+++ 17_5_lecture_7_remote/src/javascript/index.js
@@ -1,7 +1,9 @@
 //16.5.2022 Antti Suomi
+import { output } from './output.js';
 
 //page output
-
+// alert('hello world');
+document.getElementById("test").innerText = output();
 
 
 //page input
---
+++ 17_5_lecture_7_remote/src/javascript/output.js
@@ -0,0 +1,23 @@
+// 17.5.2022 Antti Suomi
+
+import { axios } from '../../node_modules/axios/dist/axios.js';
+
+axios;
+
+// const jsonString = JSON.stringify(forecast);
+
+// fs.writeFile("./forecast-data.json", jsonString, (err) => {
+//     if (err) {
+//         console.log("Error writing file", err);
+//     } else {
+//         console.log("Successfully wrote file");
+//     }
+// });
+
+// const data = require('./forecast-data.json');
+const output = (needThisHtml) => {
+
+
+}
+export { output };
+let outputJson = JSON.parse(jsonString); //* here we read the data to object(?)
