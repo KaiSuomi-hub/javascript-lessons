285ab525a9486ced4dde050589e0f215a2365416
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2022-05-18 11:25:11 +0300

    problems with react version

---
+++ 18_5_Lecture_9/tailwindexample/src/App.js
@@ -0,0 +1,21 @@
+  const Gg = () => {
+    return (
+      <p> dd </p>
+
+      )
+  }
+function App() {
+
+
+
+
+
+  return (
+    <div className="App">
+      console
+      <div><h1>Hello World</h1></div>
+    It's year <Gg />    </div>
+  )
+}
+
+export default App;
