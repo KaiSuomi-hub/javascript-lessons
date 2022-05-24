3dc374d341428892341f881085dd2504e14f84c7
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2022-05-24 12:48:37 +0300

    got around the require by importing a cdn in the index.html

--- 24-5-axios/axios.component.js
+++ 24-5-axios/axios.component.js
@@ -1,20 +1,19 @@
 //Antti Suomi 24.5
 
-const axios = require("axios");
-
+// const axios = require("axios");
+// import {axios} from "./node_modules/axios/lib/axios.js";
 const getData = async (url) => {
     const data = (await axios.get(url)).data; //here we create a new function that get's the json data
     return data;
 };
-let dd = ''
+let dd = 'ss'
 const addr = "http://127.0.0.1:8080/data.json";
 
 
 async function main() {
     const data = await getData(addr);
     console.log(data);
-	return dd;
-
+	// return data;
 }
 
 main();
