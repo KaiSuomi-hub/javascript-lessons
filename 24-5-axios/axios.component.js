e37c48560ebaca9f9a9e4063bc7eae23885dc771
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2022-05-31 09:14:09 +0300

    xx

--- 24-5-axios/axios.component.js
+++ 24-5-axios/axios.component.js
@@ -4,17 +4,32 @@ const axios = require("axios");
 // import {axios} from "./node_modules/axios/lib/axios.js";
 const getData = async (url) => { // async makes the function a promise that can be resolved
     const data = (await axios.get(url)).data; //here we create a new function that get's the json data
+    // console.log(data);
     return data;
 };
-let dd = 'ss'
 const addr = "http://127.0.0.1:8080/data.json";
 
 
+
 async function main() {
+
+    const getName = (item) => {
+    return item.employees.name;
+}
+    let i = 0;
     const data = await getData(addr);
-    let dataout = '<p>' + data.employees + '</p>';
-    console.log(dataout);
-	return dataout;
+    data.employees.forEach(function () {
+        let name = data.employees[i].name
+        // console.log(data.employees[i].name);
+        i++
+        // console.log(name)
+        return name;
+    });
+    // console.log(data.employees);
+
+    return data;
+
+
 }
 
 main();
